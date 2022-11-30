7dd0573065f0f45f62d4c906e56c976edc13ecce
samhi113 <samhiggins113@gmail.com>
2022-11-29 21:59:31 -0500

    update

---
+++ New-Site/js/samNewFooter.js
@@ -0,0 +1,9 @@
+//Javascript Document
+
+document.write("<footer> <p>Intro to Informatics</p> <p>Jay Annadurai | Sam Higgins | Noriel Arana | Ollie Heady</p> <p>(c) IUPUI 2022</p> </footer>");
+
+//<footer>
+//<p>Intro to Informatics</p>
+//<p>Jay Annadurai | Sam Higgins | Noriel Arana | Ollie Heady</p>
+//<p>(c) IUPUI 2022</p>
+//</footer>
